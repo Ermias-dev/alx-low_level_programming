b2e6b0650897510c622a05ac227478060a962ca3
Ermias-dev <edessalegne@gmail.com>
2021-07-01 05:21:41 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 6-main.c 6-cap_string.c -o 6-cap#include "holberton.h"
+
+/**
+* reverse_array - reverse the contents of an array of integers
+* @a: array of integers
+* @n: number of elements in array
+*/
+
+void reverse_array(int *a, int n)
+{
+int i;
+int h;
+
+for (i = 0; i < (n / 2); i++)
+{
+h = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = h;
+}
+}
